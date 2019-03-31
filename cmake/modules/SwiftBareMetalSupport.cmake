cb398427fd612b258e24cc59cfa5fdc2c6660220
Alan Dragomirecký <alan@dragomirecky.com>
2019-04-01 00:12:37 +0100

    automatically resolve compile paths and cleanup baremetal compiler settings

--- cmake/modules/SwiftBareMetalSupport.cmake
+++ cmake/modules/SwiftBareMetalSupport.cmake
@@ -1,28 +1,62 @@
-function (swift_baremetal_include_for_arch arch var)
-  set(includes)
+set(arm-none-eabi-gcc ${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/bin/arm-none-eabi-gcc)
 
-  if(arch STREQUAL "thumbv7m")
-    list(APPEND includes "-I${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/arm-none-eabi/include/c++/8.2.1/arm-none-eabi")
-    list(APPEND includes "-I${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/arm-none-eabi/include/c++/8.2.1")
-    list(APPEND includes "-I${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/arm-none-eabi/include")
-    list(APPEND includes "-I${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/lib/gcc/arm-none-eabi/8.2.1/include-fixed")
-    list(APPEND includes "-isysroot" "${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/arm-none-eabi")
-  else()
-    message(SEND_ERROR "unknown architecture ${arch} for baremetal")
-  endif()
+
+function(_swift_baremetal_gcc_flags_for_arch arch var)
+  set(flags)
+  list(APPEND flags "-march=${SWIFT_SDK_BAREMETAL_ARCH_${arch}_ALT_SPELLING}" "-fno-rtti" "-fno-exceptions")
+  set(${var} ${flags} PARENT_SCOPE)
+endfunction()
+
+
+function(_swift_baremetal_gcc_find program var)
+  execute_process(
+    COMMAND ${arm-none-eabi-gcc} -print-prog-name=${program}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE path)
+  set(${var} ${path} PARENT_SCOPE)
+endfunction()
+
+
+function (swift_baremetal_include_for_arch arch var)
+  _swift_baremetal_gcc_flags_for_arch(${arch} gccflags)
+
+  string(REPLACE ";" " " gccflags_ss "${gccflags}")
+  execute_process(COMMAND
+    bash -c "echo | ${arm-none-eabi-gcc} -xc++ ${gccflags_ss} -E -Wp,-v - 2>&1 1>/dev/null \
+      | grep '^ ' | sed 's/^ //g' | paste -sd\; -"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE includes)
 
   set(${var} ${includes} PARENT_SCOPE)
 endfunction()
 
+
+function(swift_baremetal_swift_compile_flags var)
+  set(${var}
+    "-Xcc" "-D_BAREMETAL"
+    "-Xfrontend" "-disable-reflection-metadata"
+    "-Xfrontend" "-assume-single-threaded"
+    "-enforce-exclusivity=none"
+    "-Xcc" "-isystem${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/arm-none-eabi/include"
+    PARENT_SCOPE)
+endfunction()
+
+
+function(swift_baremetal_c_compile_flags var)
+  set(${var}
+    "-D_BAREMETAL" "-D_GNU_SOURCE"
+    "-D_POSIX_THREADS" "-D_POSIX_READER_WRITER_LOCKS" "-D_UNIX98_THREAD_MUTEX_ATTRIBUTES"
+    "-fdata-sections" "-ffunction-sections"
+    "-fno-rtti" "-fno-exceptions"
+    "-I/Users/alandragomirecky/Projects/cvut/masters_thesis/stdlib_add"
+    PARENT_SCOPE)
+endfunction()
+
+
 function(swift_baremetal_lib_for_arch arch var)
-  set(paths)
-
-  if(arch STREQUAL "thumbv7m")
-    list(APPEND paths "${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/arm-none-eabi/lib")
-    list(APPEND paths "${SWIFT_BAREMETAL_TOOLCHAIN_PATH}/lib/gcc/arm-none-eabi/8.2.1")
-  else()
-    message(SEND_ERROR "unknown architecture ${arch} for baremetal")
-  endif()
-
+  execute_process(COMMAND
+    bash -c "${arm-none-eabi-gcc} -print-search-dirs | grep libraries | sed 's/libraries: =//g' | sed 's/:/;/g'"
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE paths)
   set(${var} ${paths} PARENT_SCOPE)
 endfunction()
