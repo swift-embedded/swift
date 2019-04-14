88d21f818292ec8023869c773f9ba0dd42b69ce3
Alan Dragomirecký <alan@dragomirecky.com>
2019-04-14 23:56:26 +0100

    simplify get_commandline on baremetal

--- stdlib/public/stubs/CommandLine.cpp
+++ stdlib/public/stubs/CommandLine.cpp
@@ -90,7 +90,7 @@ char ** _swift_stdlib_getUnsafeArgvArgc(int *outArgLen) {
   return outBuf;
 }
 #elif defined (_BAREMETAL)
-extern "C" char* get_commandline(int* buf_size);
+extern "C" char** _user_get_commandline_args(int* outArgLen) __attribute__((weak));
 SWIFT_RUNTIME_STDLIB_API
 char ** _swift_stdlib_getUnsafeArgvArgc(int *outArgLen) {
   assert(outArgLen != nullptr);
@@ -100,32 +100,12 @@ char ** _swift_stdlib_getUnsafeArgvArgc(int *outArgLen) {
     return _swift_stdlib_ProcessOverrideUnsafeArgv;
   }
 
-  int cmdline_len;
-  char* buffer = get_commandline(&cmdline_len);
-  std::vector<char*> args;
-
-  int start_pos = 0;
-  while (start_pos < cmdline_len) {
-    int end_pos = start_pos + 1;
-    // find end position
-    while (buffer[end_pos] != ' ' && end_pos < cmdline_len) {
-      end_pos ++;
-    }
-
-    // copy and append
-    char *arg = (char*)calloc(end_pos - start_pos + 1, sizeof(char));
-    memcpy(arg, buffer + start_pos, end_pos - start_pos);
-    args.push_back(arg);
-
-    // move to new start_pos
-    start_pos = end_pos + 1;
+  if (_user_get_commandline_args != nullptr) {
+    return _user_get_commandline_args(outArgLen);
   }
 
-  char **outbuf = (char **)calloc(args.size() + 1, sizeof(char *));
-  std::copy(args.begin(), args.end(), outbuf);
-  outbuf[args.size()] = nullptr;
-  *outArgLen = args.size();
-  return outbuf;
+  outArgLen = 0;
+  return nullptr;
 }
 #elif defined (_WIN32)
 #include <stdlib.h>
